c4e0cbd6ecb3c4f147472b86061b5ee636409fe5
Apple <ytapplegamer@gmail.com>
2017-04-03 15:20:25 -0400

    Temporarily fix an issue that prevented the server from starting up

--- src/pocketmine/entity/Stray.php
+++ src/pocketmine/entity/Stray.php
@@ -22,12 +22,13 @@
 
 namespace pocketmine\entity;
 
+use pocketmine\entity\hostile\Skeleton;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
 use pocketmine\network\protocol\MobEquipmentPacket;
 use pocketmine\item\Item as ItemItem;
 
-class Stray extends Skeleton{
+class Stray extends Skeleton {
 	const NETWORK_ID = 46;
 
 	public $dropExp = [5, 5];
